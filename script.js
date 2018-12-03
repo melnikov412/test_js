957a14460145fa95aeaf3c1353dcf971e4eb6b0f
Melnikov <mumontag@gmail.com>
2018-12-03 19:55:40 +0300

    Это первый коммит

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var a = 5;
+console.log(a);
+
+/**
+ * Created by Александр on 03.12.2018.
+ */
+// console.log(second);
+// let persone = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(persone.name);
+// console.log(persone["name"]);
+// console.log(persone.age);
+// console.log(persone.isMarried);
+
+// let arr = ['1.png', '2.jpg', '3.bmp'];
+
+// console.log(arr[1]);
+
+// console.log("confirm: " + confirm("Are you here?"));
+
+console.log("confirm: " + typeof(prompt("Are you here?", "Yes")));
